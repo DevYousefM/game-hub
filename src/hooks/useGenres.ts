4ad2856c15e9945a28157e2ca30591ba6bdf8c5c
capYousef <elcaptain.yousef.official@gmail.com>
2023-10-10 19:55:42 +0300

    Ship genres and platforms with app

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,10 +1,13 @@
-import useData from "./useData";
+import genres from "../data/genres";
 
 export interface Genre {
   id: number;
   name: string;
   image_background: string;
 }
+// To fetch genres from API
+// const useGenres = () => useData<Genre>("/genres");
 
-const useGenres = () => useData<Genre>("/genres");
+// To fetch genres from data folder
+const useGenres = () => ({ data: genres, isLoading: false, error: null });
 export default useGenres;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,10 +1,13 @@
-import useData from "./useData";
+import platforms from "../data/platforms";
 
 export interface Platform {
   id: number;
   name: string;
   slug: string;
 }
+// To fetch platforms from API
+// const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
 
-const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
+// To fetch platforms from data folder
+const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
 export default usePlatforms;
